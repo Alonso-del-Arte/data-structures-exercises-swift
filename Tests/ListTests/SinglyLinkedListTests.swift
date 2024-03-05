b6082ceddd3243678a5433cb0e4ae7f9a8c2e41e
Alonso del Arte <alonso.delarte@gmail.com>
2024-03-05 16:41:37 -0500

    Failing list size 0 at first test

--- Tests/ListTests/SinglyLinkedListTests.swift
+++ Tests/ListTests/SinglyLinkedListTests.swift
@@ -11,7 +11,16 @@ final class SinglyLinkedListTests: XCTestCase {
 
     func testIsEmpty() {
         let list = SinglyLinkedList<String>()
-        XCTAssertTrue(list.isEmpty(), "Newly created list should be empty")
+        let message = "Newly created list should be empty"
+        XCTAssertTrue(list.isEmpty(), message)
+    }
+    
+    func testSizeZeroAtFirst() {
+        let list = SinglyLinkedList<Date>()
+        let expected: UInt = 0
+        let actual = list.size()
+        let message = "Newly created list should be of size 0"
+        XCTAssertEqual(expected, actual, message)
     }
 
 }
