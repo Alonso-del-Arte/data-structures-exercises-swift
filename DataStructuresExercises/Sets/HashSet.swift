8b7513a93d07a45e02ffe86547de0ba9e7dc542a
Alonso del Arte <alonso.delarte@gmail.com>
2024-03-11 15:54:28 -0400

    New class, new test class

---
+++ DataStructuresExercises/Sets/HashSet.swift
@@ -0,0 +1,27 @@
+//
+//  HashSet.swift
+//  DataStructuresExercises
+//
+//  Created by Alonso del Arte on 3/11/24.
+//
+
+import Foundation
+
+struct HashSet<E : Hashable> {
+    
+    // TODO: Write tests for this
+    func isEmpty() -> Bool {
+        false
+    }
+    
+    // TODO: Write tests for this
+    func size() -> UInt {
+        UInt.max
+    }
+    
+    // TODO: Write tests for this
+    mutating func add(element: E) {
+        //
+    }
+        
+}
