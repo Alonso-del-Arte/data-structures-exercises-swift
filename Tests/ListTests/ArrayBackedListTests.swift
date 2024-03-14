72c0d9cb2e57385741decaddbc9d6c0a5488d514
Alonso del Arte <alonso.delarte@gmail.com>
2024-03-14 15:22:17 -0400

    Failing isEmpty( ) test

--- Tests/ListTests/ArrayBackedListTests.swift
+++ Tests/ListTests/ArrayBackedListTests.swift
@@ -8,28 +8,13 @@
 import XCTest
 
 final class ArrayBackedListTests: XCTestCase {
+    
+    fileprivate static let DEFAULT_TEST_INIT_CAP: UInt = 10
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func testIsEmpty() {
+        let list = ArrayBackedList<URLSession>(initialCapacity: ArrayBackedListTests.DEFAULT_TEST_INIT_CAP)
+        let message = "Newly created list should be empty"
+        XCTAssertTrue(list.isEmpty(), message)
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
+    
 }
