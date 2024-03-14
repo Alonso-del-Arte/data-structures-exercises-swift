0ce849ff299b83a0380af5903336402508aa5e7f
Alonso del Arte <alonso.delarte@gmail.com>
2024-03-14 14:49:48 -0400

    New class, new test class

---
+++ DataStructuresExercises/Lists/ArrayBackedList.swift
@@ -0,0 +1,31 @@
+//
+//  ArrayBackedList.swift
+//  DataStructuresExercises
+//
+//  Created by Alonso del Arte on 3/14/24.
+//
+
+import Foundation
+
+struct ArrayBackedList<E> {
+    
+    // TODO: Write tests for this
+    func isEmpty() -> Bool {
+        false
+    }
+    
+    // TODO: Write tests for this
+    func size() -> UInt {
+        UInt.max
+    }
+    
+    // TODO: Write tests for this
+    mutating func add(element: E) {
+        //
+    }
+    
+    init(initialCapacity: Int) {
+        //
+    }
+    
+}
