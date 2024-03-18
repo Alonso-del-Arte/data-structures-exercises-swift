667de9c8d30d68d01058764b369505258cd0f453
Alonso del Arte <alonso.delarte@gmail.com>
2024-03-18 15:47:25 -0400

    Partial rewind to failing

--- DataStructuresExercises/Sets/HashSet.swift
+++ DataStructuresExercises/Sets/HashSet.swift
@@ -13,6 +13,8 @@ struct HashSet<E : Hashable> {
     
     private var count: UInt = 0
     
+    private var buckets = [HashBucket<E>?](repeating: nil, count: 256)
+    
     func isEmpty() -> Bool {
         self.empty
     }
@@ -21,14 +23,55 @@ struct HashSet<E : Hashable> {
         self.count
     }
     
-    func contains(element: E) -> Bool {
-        !self.empty
+    private mutating func getBucket(hash: UInt8) -> HashBucket<E> {
+        let intermediate = self.buckets[Int(hash)]
+        if let unwrapped = intermediate {
+            return unwrapped
+        } else {
+            let bucket = HashBucket<E>(hash: hash)
+            self.buckets[Int(hash)] = bucket
+            return bucket
+        }
+    }
+    
+    mutating func contains(element: E) -> Bool {true
+//        let code = UInt8(truncatingIfNeeded: element.hashValue)
+//        var bucket = self.getBucket(hash: code)
+//        return bucket.contains(element: element)
     }
     
     // TODO: Write tests for this
     mutating func add(element: E) {
+        let code = UInt8(truncatingIfNeeded: element.hashValue)
+        var bucket = self.getBucket(hash: code)
+        bucket.add(element: element)
         self.empty = false
         self.count += 1
     }
     
+    private struct HashBucket<T : Hashable> {
+        
+        fileprivate let tag: UInt8
+        
+        fileprivate var items = Array<T>()
+        
+        fileprivate mutating func add(element: T) {
+            self.items.append(element)
+        }
+        
+        fileprivate mutating func remove(element: T) {
+            let index = self.items.firstIndex(of: element)
+            self.items.remove(at: index!)
+        }
+        
+        fileprivate func contains(element: T) -> Bool {
+            self.items.firstIndex(of: element) != nil
+        }
+        
+        init(hash: UInt8) {
+            self.tag = hash
+        }
+        
+    }
+        
 }
